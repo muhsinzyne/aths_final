4efa16d04c1021092e992480e82cc5b3755b532d
Muhsin Zyne MacBook Pro 16 <muhsin.zyne@gmail.com>
2022-03-22 14:43:14 +0400

    changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            //UsersSeeder::class,
+            UsersSeeder::class,
             DumpPermissions::class,
             // PermissionsSeeder::class,
             // RolesSeeder::class,
